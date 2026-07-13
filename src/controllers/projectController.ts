da8d0d09ac4239d32dff191c2ec8d3a41a52b66e
Ece Erol <ecee.eroll00@gmail.com>
2026-07-13 17:06:39 +0300

    feat: add get and delete project by ID endpoints

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -43,3 +43,49 @@ export const createProject = async (req: Request, res: Response) => {
       .json({ error: "An error occurred while creating the project." });
   }
 };
+
+// GET /projects/:id
+export const getProjectById = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  if (!id || typeof id !== "string") {
+    return res.status(400).json({ error: "Invalid project ID." });
+  }
+
+  try {
+    const project: Project | null = await prisma.project.findUnique({
+      where: { id },
+    });
+
+    if (!project) {
+      return res.status(404).json({ error: "Project not found." });
+    }
+
+    res.json(project);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the project." });
+  }
+};
+
+// DELETE /projects/:id
+export const deleteProjectById = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  if (!id || typeof id !== "string") {
+    return res.status(400).json({ error: "Invalid project ID." });
+  }
+
+  try {
+    const deletedProject = await prisma.project.delete({ where: { id } });
+    res.json({
+      message: "Project deleted successfully.",
+      project: deletedProject,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the project." });
+  }
+};
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { getProjects, createProject } from "@/controllers/projectController.js";
+import {
+  getProjects,
+  createProject,
+  getProjectById,
+  deleteProjectById,
+} from "@/controllers/projectController.js";
 
 const router = Router({ caseSensitive: true });
 
 router.get("/", getProjects);
 router.post("/", createProject);
+router.get("/:id", getProjectById);
+router.delete("/:id", deleteProjectById);
 
 export default router;
