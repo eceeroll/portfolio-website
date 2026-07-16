ec96bf09c7d44f01301b21f52a31aadb26a0a709
Ece Erol <ecee.eroll00@gmail.com>
2026-07-16 14:11:05 +0300

    feat: update project validation schema

--- backend/src/validators/projectValidator.ts
+++ backend/src/validators/projectValidator.ts
@@ -4,8 +4,9 @@ export const createProjectSchema = z.object({
   title: z.string().min(1, "Title is required"),
   description: z.string().min(1, "Description is required"),
   techStack: z.array(z.string()).min(1, "At least one technology is required"),
-  githubUrl: z.string().url("Invalid GitHub URL").optional(),
-  liveUrl: z.string().url("Invalid Live URL").optional(),
+  githubUrl: z.url("Invalid GitHub URL").optional(),
+  liveUrl: z.url("Invalid Live URL").optional(),
+  imageUrl: z.url("Invalid image URL.").optional(),
 });
 
 // all fields are optional for update, but if provided, they must meet the same validation rules
